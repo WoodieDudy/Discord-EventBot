25c2a713ed98d5d643aaf60a11c24e022a78df45
Георий Крамаренко <goshagks@yandex.ru>
2021-06-16 22:13:04 +0500

    remove negligible events

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ async def on_voice_state_update(member, before, after):
 
     if before.channel is None:
         await bot.get_channel(792288140324634634).send(f'{user} `connect` to {after.channel.name} at {time}')
-
-    elif not before.self_deaf and after.self_deaf:
-        await bot.get_channel(792288140324634634).send(f'{user} `turn off` the sound and `mute` at {time}')
-
-    elif before.self_deaf and not after.self_deaf:
-        await bot.get_channel(792288140324634634).send(f'{user} `turn on` the sound and `unmute` at {time}')
-
-    elif not before.self_mute and after.self_mute:
-        await bot.get_channel(792288140324634634).send(f'{user} `mute` himself at {time}')
-
-    elif before.self_mute and not after.self_mute:
-        await bot.get_channel(792288140324634634).send(f'{user} `unmute` himself at {time}')
+    #
+    # elif not before.self_deaf and after.self_deaf:
+    #     await bot.get_channel(792288140324634634).send(f'{user} `turn off` the sound and `mute` at {time}')
+    #
+    # elif before.self_deaf and not after.self_deaf:
+    #     await bot.get_channel(792288140324634634).send(f'{user} `turn on` the sound and `unmute` at {time}')
+    #
+    # elif not before.self_mute and after.self_mute:
+    #     await bot.get_channel(792288140324634634).send(f'{user} `mute` himself at {time}')
+    #
+    # elif before.self_mute and not after.self_mute:
+    #     await bot.get_channel(792288140324634634).send(f'{user} `unmute` himself at {time}')
 
     elif not before.self_stream and after.self_stream:
         await bot.get_channel(792288140324634634).send(f'{user} `start stream` at {time}')
@@ -45,9 +45,9 @@ async def on_voice_state_update(member, before, after):
 
     elif after.channel is None:
         await bot.get_channel(792288140324634634).send(f'{user} `leaves` us at {time}')
-
-    else:
-        await bot.get_channel(792288140324634634).send(f'{user} do something')
+    #
+    # else:
+    #     await bot.get_channel(792288140324634634).send(f'{user} do something')
 
 
 bot.run(settings['token'])
